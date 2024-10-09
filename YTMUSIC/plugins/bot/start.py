8735433eabf2a8380d48e6b3edd38182dd04bcfe
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-09 20:38:02 +0530

    Update start.py

--- YTMUSIC/plugins/bot/start.py
+++ YTMUSIC/plugins/bot/start.py
@@ -88,6 +88,97 @@ async def start_pm(client, message: Message, _):
                 channel = result["channel"]["name"]
                 link = result["link"]
                 published = result["publishedTime"]
+import time
+import random
+import asyncio
+from pyrogram import filters
+from pyrogram.errors import ChannelInvalid
+from YTMUSIC.plugins.play.playlist import del_plist_msg
+from pyrogram.enums import ChatType, ChatMembersFilter
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
+
+import config
+from YTMUSIC import app
+from YTMUSIC.misc import _boot_
+from YTMUSIC.plugins.sudo.sudoers import sudoers_list
+from YTMUSIC.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+    connect_to_chat,
+)
+from YTMUSIC.utils.decorators.language import LanguageStart
+from YTMUSIC.utils.formatters import get_readable_time
+from YTMUSIC.utils.inline import help_pannel, private_panel, start_panel
+from config import BANNED_USERS
+from strings import get_string
+
+
+async def delete_message_after_delay(message, delay):
+    await asyncio.sleep(delay)
+    await message.delete()
+
+
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@LanguageStart
+async def start_pm(client, message: Message, _):
+    await add_served_user(message.from_user.id)
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+
+        if name[0:3] == "del":
+            await del_plist_msg(client=client, message=message, _=_)
+
+        if name[0:4] == "help":
+            keyboard = help_pannel(_)
+            return await message.reply_photo(photo=config.START_IMG_URL,
+                caption=_["help_1"].format(config.SUPPORT_CHAT),
+                reply_markup=keyboard,
+            )
+        if name[:8] == "connect_":
+            chat_id = name[8:]
+            try:
+                title = (await app.get_chat(chat_id)).title
+            except ChannelInvalid:
+                return await message.reply_text(f"Looks like I am not an admin of the chat ID {chat_id}")
+            
+            admin_ids = [member.user.id async for member in app.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)]
+            if message.from_user.id not in admin_ids:
+                return await message.reply_text(f"Sorry, but it seems you are not an admin of {title}.")
+            a = await connect_to_chat(message.from_user.id, chat_id)
+            if a:
+                await message.reply_text(f"You are now connected to {title}.")
+            else:
+                await message.reply_text(a)
+
+        
+        if name[0:3] == "sud":
+            await sudoers_list(client=client, message=message, _=_)
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} started the bot to check <b>Sudo List</b>.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
+                )
+            return
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = await VideosSearch(query, limit=1).next()
+            for result in results["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
@@ -112,41 +203,41 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} started the bot to check <b>Track Information</b>.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
                 )
     else:
-out = private_panel(_)
-    baby = await message.reply_text(f"**🄻 10%**")
-    await asyncio.sleep(0.5)  # Delay in seconds
-    await baby.edit_text(f"**🄻🄾 30%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰 30%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳 30%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸 50%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽 30%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 70%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱 30%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰 90%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰🄱 90%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰🄱🅈 100%**")
-    await asyncio.sleep(0.5)
-    await baby.edit_text(f"**ɴᴏᴡ sᴛᴀʀᴛᴇᴅ..**")
-    await baby.delete()
-    
-    await message.reply_photo(photo=config.START_IMG_URL,
-    caption=_["start_2"].format(message.from_user.mention, app.mention),
-    reply_markup=InlineKeyboardMarkup(out),
-    )
-    if await is_on_off(2):
-        return await app.send_message(
-            chat_id=config.LOGGER_ID,
-            text=f"{message.from_user.mention} started the bot.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
+        out = private_panel(_)
+        baby = await message.reply_text(f"**🄻 10%**")
+        await asyncio.sleep(0.5)  # Delay in seconds
+        await baby.edit_text(f"**🄻🄾 30%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰 30%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳 30%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸 50%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽 30%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 70%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱 30%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰 90%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰🄱 90%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**🄻🄾🄰🄳🄸🄽🄶 🄱🄰🄱🅈 100%**")
+        await asyncio.sleep(0.5)
+        await baby.edit_text(f"**ɴᴏᴡ sᴛᴀʀᴛᴇᴅ..**")
+        await baby.delete()
+
+        await message.reply_photo(photo=config.START_IMG_URL,
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(out),
         )
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"{message.from_user.mention} started the bot.\n\n<b>User ID:</b> <code>{message.from_user.id}</code>\n<b>Username:</b> @{message.from_user.username}",
+            )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
