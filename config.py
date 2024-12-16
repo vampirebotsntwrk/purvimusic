dfc6d894c4e99f54e77e1394b954d09733088235
legendtosif <toofanxd72@gmail.com>
2024-12-17 00:30:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ________________________________________________________________________________#
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LEGENDMICKEY/SPOTIFY",
+    "https://github.com/legendtosif/Spotify",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
