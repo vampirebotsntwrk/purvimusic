918f5ea3ced3d90e6554308b859bc0eda425e23b
ISTKHAR-king <istkharalam225@gmail.com>
2024-12-25 22:31:00 +0530

    Update gadd.py

--- YTMUSIC/plugins/tools/gadd.py
+++ YTMUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7520092354"
+OWNERS = "7995626429"
 from YTMUSIC import app
 from YTMUSIC.utils.database import add_served_chat, get_assistant
 
