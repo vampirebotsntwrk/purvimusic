d9400b2cba4a9d927b1cdb682f9d571fb1c77ee4
VAMPIRE BOTS <vampirebots27@gmail.com>
2025-07-30 13:20:51 +0530

    Update gadd.py

--- YTMUSIC/plugins/tools/gadd.py
+++ YTMUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7995626429"
+OWNERS = "8051082678"
 from YTMUSIC import app
 from YTMUSIC.utils.database import add_served_chat, get_assistant
 
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002011723196:
+            if dialog.chat.id == -1002830767280:
                 continue
             try:
 
